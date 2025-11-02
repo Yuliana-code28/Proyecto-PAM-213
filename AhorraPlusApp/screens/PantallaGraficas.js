afc6f47a1789a1a451a69519d3ff03b37aa0e125
Victor-Atanacio <124050394@upq.edu.mx>
2025-11-02 14:58:36 -0600

    Agregue un flexDirection para los iconos

--- AhorraPlusApp/screens/PantallaGraficas.js
+++ AhorraPlusApp/screens/PantallaGraficas.js
@@ -131,6 +131,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems:"flex-start",
+    flexDirection:"row",
     paddingTop:40,
     paddingLeft:30,
     paddingRight:30
