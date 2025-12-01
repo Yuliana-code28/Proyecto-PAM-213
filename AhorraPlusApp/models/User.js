1e416f93c3591558019b98b65654ecb77b3b30b1
Yuliana-code28 <124048858@upq.edu.mx>
2025-11-30 20:02:26 -0600

    Validación contraseña 1 mayúscula y 1 número

--- AhorraPlusApp/models/User.js
+++ AhorraPlusApp/models/User.js
@@ -8,9 +8,21 @@ export class User {
     }
 
     static validar(nombre, correo, password) {
-        if (!nombre || nombre.trim().length === 0) throw new Error('El nombre es obligatorio');
-        if (!correo || !correo.includes('@')) throw new Error('Correo invalido');
-        if (!password || password.length < 8) throw new Error('La contraseña es muy corta');
+        if (!nombre || nombre.trim().length === 0) 
+            throw new Error('El nombre es obligatorio');
+        
+        if (!correo || !correo.includes('@')) 
+            throw new Error('Correo invalido');
+        
+        if (!password) 
+            throw new Error('La contraseña es obligatoria');
+
+        // Validación para que la contraseña tenga mínimo 8 caracteres, 1 mayúscula y 1 número
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+        if (!passwordRegex.test(password)) {
+            throw new Error('La contraseña debe tener al menos 8 caracteres, incluir una mayúscula y un número');
+        }
+
         return true;
     }
 }
