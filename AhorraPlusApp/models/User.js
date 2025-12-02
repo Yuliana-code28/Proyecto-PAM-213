03563c2b029b14bdb81df7d6b8b9d75230e58213
JesussDelgadoo <122041657@upq.edu.mx>
2025-12-02 12:56:44 -0600

    Regex del Correo

--- AhorraPlusApp/models/User.js
+++ AhorraPlusApp/models/User.js
@@ -11,13 +11,14 @@ export class User {
         if (!nombre || nombre.trim().length === 0) 
             throw new Error('El nombre es obligatorio');
         
-        if (!correo || !correo.includes('@')) 
-            throw new Error('Correo invalido');
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!correo || !emailRegex.test(correo)) {
+            throw new Error('El formato del correo es inválido');
+        }
         
         if (!password) 
             throw new Error('La contraseña es obligatoria');
 
-        // Validación para que la contraseña tenga mínimo 8 caracteres, 1 mayúscula y 1 número
         const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
         if (!passwordRegex.test(password)) {
             throw new Error('La contraseña debe tener al menos 8 caracteres, incluir una mayúscula y un número');
